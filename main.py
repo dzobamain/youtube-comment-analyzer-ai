7fd3de8d61817a4ad9e0291927935efe491cbc81
Dzobamain <knopkayt@gmail.com>
2025-02-18 19:50:19 +0100

    feat(main): added YouTube video comment fetching (#2)

--- main.py
+++ main.py
@@ -1,5 +1,49 @@
-def main():
-    print("youtube-comment-analyzer-ai-py")
+from googleapiclient.discovery import build
+import json
+import sys
 
-if __name__ == '__main__':
+api_key = "" # API key
+youtube = build("youtube", "v3", developerKey=api_key)
+
+
+def get_video_comments(video_id):
+    comments = []
+    request = youtube.commentThreads().list(
+        part="snippet",
+        videoId=video_id,
+        maxResults=100
+    )
+    response = request.execute()
+
+    while response:
+        for item in response['items']:
+            comment = item['snippet']['topLevelComment']['snippet']['textOriginal']
+            comments.append(comment)
+        if 'nextPageToken' in response:
+            request = youtube.commentThreads().list(
+                part="snippet",
+                videoId=video_id,
+                pageToken=response['nextPageToken'],
+                maxResults=100
+            )
+            response = request.execute()
+        else:
+            break
+
+    return comments
+
+def main():
+    video_url = "" # URL
+    if 'v=' not in video_url or video_url == None:
+        return
+    video_id = video_url.split('v=')[1].split('&')[0]
+    
+    comments = get_video_comments(video_id)
+    
+    print("Comments:\n")
+    for comment in comments:
+        print("\t" + comment)
+
+
+if __name__ == "__main__":
     main()
