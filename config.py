b2f21178802541e88e536c333543a1e3a0259170
Dzobamain <knopkayt@gmail.com>
2025-03-06 20:15:25 +0100

    feat(main): add language parameter for GPT

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 api_key_youtube = os.getenv("YOUTUBE_API_KEY")
 api_key_openai = os.getenv("OPENAI_API_KEY")
-model_gpt = "gpt-4o"
+model_gpt = "gpt-3.5-turbo"
 task_settings = read_from_file("apis/task_settings.txt")
 chat_log_file_name = "chat_history.txt"
 
